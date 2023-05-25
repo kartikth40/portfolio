3dec27475f0df47783a659dd7dd196c80362539e
kartikth40 <kartikth40@gmail.com>
2023-05-25 12:51:08 +0530

    fixed some warnings

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react'
-import Contact from '../Contact/index'
 import useWindowSize from '../../brain/useWindowSize'
 
 import { size as devSize } from '../../juice/mediaQueries'
